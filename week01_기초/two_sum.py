389740ad20e86ef24b450b6c576e1162cef33966
JamesLoganLee <126999122+JamesLoganLee@users.noreply.github.com>
2026-07-06 10:03:03 +0900

    Create two_sum.py

---
+++ week01_기초/two_sum.py
@@ -0,0 +1,10 @@
+# [프로그래머스 입문] 두 수의 합
+# 정수 a, b가 주어질 때 두 수의 합을 return
+
+def solution(a, b):
+    answer = a + b
+    return answer
+
+# 테스트
+print(solution(3, 5))   # 8
+print(solution(10, 20)) # 30
